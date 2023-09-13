054eea587cea0d98b14715792746565d328114d0
JoseLucasdevv <joselucas.dev@gmail.com>
2023-09-13 10:53:49 -0300

    add file config database
    	modified:   src/config/db.js

---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+//  # the file is responsible for make connection with database.
+
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.connect_String_URL,
+});
+
+pool.on("error", (err, client) => {
+  console.log("Unexpected Error on idle client", err);
+  process.exit(-1);
+});
+
+pool.on("connect", (client) => {
+  console.log(client);
+  console.log("database connect");
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
